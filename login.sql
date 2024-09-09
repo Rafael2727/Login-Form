038a7e05bdc2f64892cc25670b6853fc8b714107
Rafael2727 <rafaelbatnag27@gmail.com>
2024-09-09 09:37:14 +0800

    first commit

---
+++ login.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE loginpage;
+
+CREATE TABLE Persons (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
